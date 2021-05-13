baa285281181ef9a005c296e595f381bb3e4c159
uglygus <cooperbattersby@gmail.com>
2021-05-13 17:33:50 -0400

    some more lazy changes

--- safenames.py
+++ safenames.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 """
-
 safenames.py
 Walks a directory tree and tests each file and directory for cross platform legality.
 Requires user confirmation before any changes are made.
 
 Tested on Mac, Linux and OSX
-
 """
 
 import os
@@ -29,7 +27,6 @@ except ImportError:
 else:
     def getch():
         """getch() -> key character
-
         Read a single keypress from stdin and return the resulting character.
         Nothing is echoed to the console. This call will block if a keypress
         is not already available, but will not wait for Enter to be pressed.
@@ -55,11 +52,12 @@ BAD_WINDOWS_CHARS = r':<>"\/|?*' + '\n' + '\r' + '\x7F'
 for x in range(0, 31):
     BAD_WINDOWS_CHARS += chr(x)
 
-BAD_LINUX_CHARS = "/" + "\00"
-BAD_MAC_CHARS = ":" + "\00"
+BAD_LINUX_CHARS = "/"
+BAD_MAC_CHARS = ":"
 BAD_IDEAS_CHARS = '\t'
 
-bad_chars_all = BAD_WINDOWS_CHARS + BAD_LINUX_CHARS + BAD_MAC_CHARS + BAD_IDEAS_CHARS
+BAD_CHARS_ALL = BAD_WINDOWS_CHARS + \
+    BAD_LINUX_CHARS + BAD_MAC_CHARS + BAD_IDEAS_CHARS
 
 # must be uppercase
 BAD_WINDOWS_NAMES = ['CON', 'PRN', 'AUX', 'NUL', 'CLOCK$',
@@ -69,6 +67,7 @@ BAD_WINDOWS_NAMES = ['CON', 'PRN', 'AUX', 'NUL', 'CLOCK$',
                      'PAGEFILE.SYS', '$SECURE', '$UPCASE', '$VOLUME', '$EXTEND',
                      ]
 
+# not strictly prohibited but just terrible names for files
 BAD_IDEAS_NAMES = ['-']
 
 commandline_args = None
@@ -81,24 +80,13 @@ def debug(message):
         print(message)
 
 
-def is_bad_char(filename, charlist):
-    """ Return true if filename contains a bad character from the charlist. """
-    debug('is_bad_char({}, {})'.format(filename, charlist))
-    for char in charlist:
-        if char in filename:
-            print("illegal__{}__".format(char))
-            return char
-
-    return False
-
-
 def ends_in_white_space(item):
     """ Return true if the string ends in whitespace. """
+
     debug('ends_in_white_space(item={})'.format(item))
     if item == item.rstrip():
         return False
-    else:
-        return True
+    return True
 
 
 def printable(char):
@@ -128,6 +116,7 @@ def printable(char):
 
 def type_newname(item, root):
     """ manually type a new filename. """
+
     debug('type_newname(item={}, root={})'.format(item, root))
 
     print(os.path.join(root, item))
@@ -146,30 +135,38 @@ def type_newname(item, root):
 
 def collapsewhite(filename):
     """ Returns filename with whitespace stripped and collapsed. """
+
+    messages = []
+
     debug('collapsewhite(filename="{}")'.format(filename))
-    filename = ' '.join(filename.split())
+    newfilename = ' '.join(filename.split())
+    if newfilename != filename:
+        messages.append('collapsed whitespace')
     debug('collapsewhite(filename="{}")'.format(filename))
-    return filename
+    return newfilename, messages
 
 
 def replace_bad_chars(filename, bad_chars):
     """ Returns filename with bad characters replaced with underscores. """
-    
+
+    messages = []
+
     debug('replace_bad_chars(filename="{}" bad_chars="{}")'.format(
         filename, bad_chars))
     for c in bad_chars:
-        if (c in filename):
-            print('\tbad character "{}"'.format(printable(c)))
+        if c in filename:
+            messages.append('bad character "{}"'.format(printable(c)))
+
             filename = filename.replace(c, '_')
     debug('replace_bad_chars returning="{}"'.format(filename))
-    return filename
+
+    return filename, messages
 
 
 def name_exists(item):
-    """
-    Returns new suggested name if name already exists.
-    Appends a version number in parenthesis.
-    ie. newfilename (1).txt
+    """ Returns new suggested name if name already exists.
+        Appends a version number in parenthesis.
+        ie. 'newfilename (1).txt'
     """
 
     debug('name_exists(item={})'.format(item))
@@ -195,129 +192,274 @@ def name_exists(item):
 
             new = root + ext
         return new
-    else:
-        return item
+
+    return item
 
 
 def rename_item(old, new):
     """ Rename file. """
+
     debug('rename_item(old={}, new={})'.format(old, new))
     new = name_exists(new)
-    print('old="' + old + '",  new="' + new)
+    #print('old="' + old + '",  new="' + new)
     try:
         os.rename(old, new)
     except OSError as e:
         print('OSError!', e)
 
 
-def clean_item(item, root):
+def only_whitespace(fname):
+    """ tests if filename is whitespace only returns '_' as new filename.
+        tests extension too.
     """
-    Given a filename (item)
-    Returns new filename with all corrections applied.
+    debug('only_whitespace(fname={})'.format(fname))
+
+    messages = []
+
+    file_nameonly, file_ext = os.path.splitext(fname)
+
+    file_nameonly = file_nameonly.rstrip()
+    if file_nameonly == '':
+        filenameonly = "_"
+        messages.append('filename is only whitespace')
+
+    fname_stripped = file_nameonly + file_ext
+
+    if fname_stripped != fname:
+        messages.append('trailing whitespace')
+
+    return fname_stripped, messages
+
+
+def bad_windows_name(fname):
+    """ illegal windows names
+    """
+
+    debug('bad_windows_name(fname={})'.format(fname))
+    messages = []
+
+    if fname.upper() in BAD_WINDOWS_NAMES:
+        messages.append(fname + ' is a reserved name on Windows')
+        fname = fname + '_legal'
+
+    return fname, messages
+
+
+def bad_ideas_name(fname):
+    """ catch names that you shouldn't be using regardless like '-'.
+    """
+
+    debug('bad_ideas_name(fname={})'.format(fname))
+    messages = []
+
+    if fname.upper() in BAD_IDEAS_NAMES:
+        messages.append(fname + ' is a BAD idea for a filename')
+        fname = fname + '_better_name'
+
+    return fname, messages
+
+
+def has_warning(item, root):
+    """ Returns True if the filename is a known system name and should not be changed.
+        This list is incomplete and should be expanded.
+    """
+    
+    debug('has_warning(item={}, root={})'.format(item,root))
+    messages = []
+
+    if item == "Icon\r":
+        messages.append(
+                'WARNING: "Icon\\r" : Not Windows compatible but MacOS system filename. Will not change.')
+        return True, messages
+
+
+    if '/Photos Library.photoslibrary' in root:
+        messages.append('WARNING: "{}" is part of the MacOS Photos App Library. Will not change.'.format(item))
+        return True, messages
+
+    if ':' in item and (item.endswith('.abcdg') or item.endswith('.abcdi') or item.endswith('.abcdp') or item.endswith('.abcds')):
+        messages.append(
+                'WARNING: "{}" contains an illegal colon but MacOS Address Book file. Will not change.'.format(item))
+        return True, messages
+
+    if '.AppleDouble' in root:
+        if '.Parent::EA' in item or '::EA::' in item or '::EA' in item:
+            messages.append(
+                    'WARNING: "{}" contains an illegal colon but is an MacOS Appledouble file. Will not change.'.format(item))
+            return True, messages
+
+    return False, messages
+
+def trailing_whitespace(fname):
+    """ strips trailing whitespace """
+    debug('trailing_whitespace(fname={})'.format(fname))
+
+    messages = []
+
+    file_nameonly, file_ext = os.path.splitext(fname)
+
+    fname_stripped = file_nameonly.rstrip() + file_ext.rstrip()
+
+    if fname_stripped != fname:
+        messages.append('trailing whitespace')
+
+    return fname_stripped, messages
+
+
+def leading_whitespace(fname):
+    """ leading whitespace
+    """
+    debug('leading_whitespace(fname={})'.format(fname))
+
+    messages = []
+
+    file_nameonly, file_ext = os.path.splitext(fname)
+
+    fname_stripped = file_nameonly.lstrip() + file_ext.lstrip()
+
+    if fname_stripped != fname:
+        messages.append('leading whitespace')
+
+    return fname_stripped, messages
+
+
+def print_messages(messages):
+    """ messages is a list of lists. prints each item in each list"""
+    for m in messages:
+        if isinstance(m, list):
+            for n in m:
+                print('\t{}'.format(n))
+        else:
+            print('\t{}'.format(m))
+
+
+def clean_item(item, root):
+    """ Given a filename (item)
+        Returns new filename with all corrections applied.
     """
     debug('clean_item(item={}, root={})'.format(item, root))
+
     old = os.path.join(root, item)
+
+    messages = []
 
     if commandline_args.verbose:
         print(os.path.join(root, item))
 
     cleaned = item
 
-    if item == "Icon\r":
-        # - we should this back on if we add a --verbose flag
-        # print('"Icon\\r" : Not Windows compatible but OSX system filename. Will not change.')
-        return
-
-    if ':' in item and (item.endswith('.abcdg') or item.endswith(
-            '.abcdi') or item.endswith('.abcdp') or item.endswith('.abcds')):
-            # - we should turn this back on if we add a --verbose flag
-            # print('"{}" contains an illegal colon but OSX Address Book file. Will not change.'.format(item))
-        return
-
-    if '.AppleDouble' in root:
-        if '.Parent::EA' in item or '::EA::' in item or '::EA' in item:
-            # - we should turn this back on if we add a --verbose flag
-            # print('"{}" contains an illegal colon but is an OSX Appledouble file. Will not change.'.format(item))
-            return
-
     item_clean = item
 
-    item_clean = replace_bad_chars(item_clean, bad_chars_all)
+    item_clean, new_messages = bad_windows_name(item_clean)
+    messages.append(new_messages)
+
+    item_clean, new_messages = replace_bad_chars(item_clean, BAD_CHARS_ALL)
+    messages.append(new_messages)
+
+    item_clean, new_messages = only_whitespace(item_clean)
+    messages.append(new_messages)
+
+    item_clean, new_messages = trailing_whitespace(item_clean)
+    messages.append(new_messages)
+
+    item_clean, new_messages = leading_whitespace(item_clean)
+    messages.append(new_messages)
 
     if commandline_args.collapsewhite:
-        item_clean = collapsewhite(item_clean)
+        item_clean, new_messages = collapsewhite(item_clean)
+        messages.append(new_messages)
+
+
+
+    warn_bool, warn_str = has_warning(item, root)
+#     print('item=',item)
+#     print('warn_bool=', warn_bool)
+#     print('warn_str=', warn_str)
+
+    messages.append(warn_str)
+#     if warn_bool == True:
+#         print_messages(messages)
+#         return
+
+
 
     if item_clean != item:
-        item_clean = name_exists(os.path.join(root, item_clean))
 
-        xxx, item_clean = os.path.split(item_clean)
+        print('\n{}'.format(os.path.abspath(os.path.join(root, item))))
 
-        print('{}/{}'.format(root, item)), end='', flush=True)
-        print('replace "{}" with "{}"?  (Y/n/t/x : Yes/no/type/delete): '.format(item, item_clean), end='',  flush=True)
+        print_messages(messages)
 
-        ch = getch()
-        print(ch)
+        if not warn_bool:
 
-        if ch.lower() == 'y' or ch == '\r':
-            new = os.path.join(root, item_clean)
-            # print ('old=', old, '\nnew=', new)
-            rename_item(old, new)
-            # print('renamed!')
-            cleaned = item_clean
+            print('Replace "{}" with "{}"?  (Y/n/t/x : Yes/no/type/delete): y'.format(item,
+                                                                                      item_clean), end='', flush=True)
 
-        if ch.lower() == 'x':
-            os.unlink(os.path.join(root, item))
+            ch = getch()
+            print(ch)
 
-        if ch.lower() == 't':
-            new_name = input("new filename: ")
-            new = os.path.join(root, new_name)
-            # print ('old=', old, '\nnew=', new)
-            rename_item(old, new)
+            if ch.lower() == 'y' or ch == '\r':
+                new = os.path.join(root, item_clean)
+                # print ('old=', old, '\nnew=', new)
+                rename_item(old, new)
+                # print('renamed!')
+                cleaned = item_clean
 
-    fname, ext = os.path.splitext(item)
+            if ch.lower() == 'x':
+                os.unlink(os.path.join(root, item))
 
-    if ends_in_white_space(fname):
-
-        item_stripped = fname.rstrip()
-        if item_stripped == '':
-            item_stripped = "_"
-            print('file "{}" is only whitespace!'.format(root))
-
-        print('{}/{}'.format(root, fname))
-        # , end='', flush=True)
-
-        print('strip trailing whitespace? (Y/n/t/x : Yes/no/type/delete)', end='',  flush=True)
-
-        ch = getch().lower()
-        print(ch)
-
-        if ch == 'x':
-            os.unlink(os.path.join(root, item))
-
-        if ch == 't':
-            new_name = input("new filename: ")
-            new = os.path.join(root, new_name)
-            # print ('old=', old, '\nnew=', new)
-            rename_item(old, new)
-            cleaned = new
-
-        if ch.lower() == 'y' or ch == '\r':
-            print('renaming "{}" --> "{}"'.format(item, item_stripped + ext))
-            rename_item(os.path.join(root, item),
-                        os.path.join(root, item_stripped + ext))
-            cleaned = item_stripped + ext
+            if ch.lower() == 't':
+                new_name = input("new filename: ")
+                new = os.path.join(root, new_name)
+                # print ('old=', old, '\nnew=', new)
+                rename_item(old, new)
 
     return cleaned
 
 
+def check_for_case_only_dupes(all_items, root):
+    """ check if any of the file or dir names differ by case only and correct it
+    """
+
+    # check for duplicates differing only by case
+    all_items_casefold = [x.casefold() for x in all_items]
+
+    dupes = []
+
+    # print('all_items_casefold==',all_items_casefold)
+    for item in all_items_casefold:
+        if all_items_casefold.count(item) > 1:
+            #print('duplicate', item)
+
+            for x in all_items:
+                if x.casefold() == item:
+                   # print('DUPE=', x)
+                    dupes.append(x)
+
+    #print('dupes=', dupes)
+    dupes_set = set(dupes)
+    # convert the set to the list
+    unique_dupes = (list(dupes_set))
+    if len(unique_dupes) > 0:
+        print('WARNING: ', root, ' contains duplicates differing by case:')
+        for x in unique_dupes:
+            print('\t', os.path.join(root, x))
+
+
 def main():
     """ Do the main thing. """
-    #    global log_debug
+
     global commandline_args
+
+    #warnings need a couple settings
+        # heed warnings and prevent user from making changes (default)
+        # ignore warnings and allow user to make changes
+
+
 
     parser = argparse.ArgumentParser(
         description="Catch and correct directory and filenames that are not cross compatible. ")
-    parser.add_argument("dir", help='directory to clean')
+    parser.add_argument("input", nargs='*',
+                        default=None, help="accepts files and/or folders to be cleaned")
     parser.add_argument('-c', '--collapsewhite', action='store_true',
                         help='collapse multple white spaces into one " " also trims any leading and trailing whitespace')
     parser.add_argument('--debug', action='store_true', help='more debug info')
@@ -332,26 +474,47 @@ def main():
     debug('commandline_args.verbose = {}'.format(commandline_args.verbose))
     debug('commandline_args.debug   = {}'.format(commandline_args.debug))
 
-    for directory in [commandline_args.dir]:
-        for root, dirs, files in os.walk(directory, topdown=False):
+    if not commandline_args.input:
+        parser.print_help()
+        return 0
 
-            all_items = dirs + files
+    debug('commandlines_arg ="{}"'.format(commandline_args.input))
 
-            for item in all_items:
-                if item.upper() in BAD_WINDOWS_NAMES:
-                    type_newname(item, root)
 
-                if item.upper() in BAD_IDEAS_NAMES:
-                    type_newname(item, root)
+    # process files and dirs
+    for single_input in commandline_args.input:
 
-                item_cleaned = clean_item(item, root)
-                # while item:
-                # item = clean_item(item, root)
+        if not (os.path.isdir(single_input) or os.path.isfile(single_input)):
+            print('ERROR: input is not a file or a directory: ' + single_input)
+            parser.print_help()
+            return 0
 
-                debug(
-                    'item="{}", item_cleaned="{}"'.format(
+        absinput = os.path.abspath(single_input)
+
+        if os.path.isfile(absinput):
+            root, item = os.path.split(absinput)
+            debug('root="{}", item="{}"'.format(root, item))
+            item_cleaned = clean_item(item, root)
+            debug('item="{}", item_cleaned="{}"'.format(item, item_cleaned))
+
+        if os.path.isdir(absinput):
+            for root, dirs, files in os.walk(absinput, topdown=False):
+
+                all_items = dirs + files
+
+                # check for duplicates differing only by case
+                check_for_case_only_dupes(all_items, root)
+
+                # clean the items
+                for item in all_items:
+                    debug('item="{}"'.format(item))
+                    item_cleaned = clean_item(item, root)
+                    debug('item="{}", item_cleaned="{}"'.format(
                         item, item_cleaned))
+
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    return_code = main()
+    sys.exit(return_code)
